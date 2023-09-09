7568d094368c9ad08ad93988a0fad448722a7953
Jack Moran <jvckmorvn@gmail.com>
2023-09-09 23:04:21 +0100

    styling

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,4 +1,6 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { GlobalStyles } from '../../constants/styles';
 
 export default function ExpensesSummary({expenses, periodName}) {
   const expensesSum = expenses.reduce((sum, expense) => {
@@ -6,9 +8,29 @@ export default function ExpensesSummary({expenses, periodName}) {
   }, 0);
   
   return (
-    <View>
-      <Text>{periodName}</Text>
-      <Text>£{expensesSum.toFixed(2)}</Text>
+    <View style={styles.container}>
+      <Text style={styles.period}>{periodName}</Text>
+      <Text style={styles.sum}>£{expensesSum.toFixed(2)}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colours.primary50,
+    borderRadius: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  period: {
+    fontSize: 12,
+    color: GlobalStyles.colours.primary400
+  },
+  sum: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: GlobalStyles.colours.primary500
+  }
+});
